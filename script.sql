7b49581db6afb761a27cd2743654ff573401df2f
Juubaju <julia.pereira133@etec.sp.gov.br>
2022-03-23 21:22:23 -0300

    scripts do banco de dados

---
+++ script.sql
@@ -0,0 +1,19 @@
+Drop database Acougue;
+create database Acougue;
+
+Drop table Produtos;
+create table Produtos (Nome varchar(50), Preco int);
+
+Drop table Funcionários;
+create table Funcionários(Nome varchar(50), Registro int);
+
+Drop table Fornecedor;
+create table Fornecedor(Nome varchar(50), Registro int, Produtos varchar(50));
+
+Drop table Cliente;
+create table Cliente(Nome varchar(50), CPF int, Endereço varchar(20));
+
+
+
+
+
